a857833cc042b9095c1e784017aa9133994720f6
pilar-am <pilimorenoperez@gmail.com>
2022-12-01 17:33:36 +0100

    AnadirProductos y dependenc en pom

---
+++ src/main/java/proyecto/Main.java
@@ -0,0 +1,12 @@
+package proyecto;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Proyecto funciona??");
+		
+	}
+
+}
